3b9fd09ac6bb986a7e9c6a2ef8cee2c63b9c9ba3
SanjuBilly <sanjana22021998@gmail.com>
2016-09-17 20:21:18 +0530

    Add score for boomerang

--- src/utils/scoreboard.js
+++ src/utils/scoreboard.js
@@ -11,6 +11,9 @@ class Scoreboard {
     if (enemyType === "basic") {
       this.game.global.score += 50;
     }
+    else if(enemyType =="boomerang"){
+      this.game.global.score += 70;
+    }
   }
 
   die() {
